89d625cda56f52e3244a8f31339bd9b8e5adce1e
Shimpei Otsubo <potsbo@gmail.com>
2017-06-19 17:19:48 +0900

    Install quiver

--- provisioning/run.sh
+++ provisioning/run.sh
@@ -9,4 +9,9 @@ if [ -n "${TMUX:-}" ] && command -v $RTUN > /dev/null 2>&1; then
 	CMD=($RTUN ${CMD[@]})
 fi
 
-${CMD[@]} playbook.yml
+ARGS=('')
+for a in "$@"; do
+	ARGS=(${ARGS[@]} "$a")
+done
+
+${CMD[@]} ${ARGS[@]} playbook.yml
