2840f643c468c7aad8cee1759103ea0dd3f36ca2
Shimpei Otsubo <potsbo@gmail.com>
2024-06-29 15:48:07 +0900

    Update

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.cmd("source ~/.vim/rc/move.vimrc")
+
+vim.api.nvim_create_user_command("CommandP", "Telescope find_files", {})
