60a756843f771f3956ae7cce208a9fb9d22fbc78
Shimpei Otsubo <potsbo@gmail.com>
2024-07-07 14:41:26 +0900

    Fix jk

--- config/.config/nvim/lua/plugins/neo-tree.lua
+++ config/.config/nvim/lua/plugins/neo-tree.lua
@@ -4,10 +4,9 @@ return {
     enable_cursor_hijack = true,
     window = {
       mappings = {
-        ["t"] = "move_cursor_up",
-        ["h"] = "move_cursor_down",
-        ["n"] = "focus_preview",
-        ["z"] = "focus_preview",
+        -- cancel default config to allow dvorak jk moves
+        ["t"] = "",
+        ["h"] = "",
       },
     },
   },
