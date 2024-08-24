aecd5b954e85b74a51f6318d5052ad5067480658
Shimpei Otsubo <potsbo@gmail.com>
2024-08-24 14:06:32 +0900

    Add nvim wl-clipboard support

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -5,3 +5,27 @@ vim.api.nvim_create_user_command("CommandShiftF", function()
   require("conform").format({ async = true, lsp_format = "fallback" })
 end, { range = true })
 vim.api.nvim_create_user_command("CommandP", "Telescope find_files", {})
+
+-- https://stackoverflow.com/questions/75548458/copy-into-system-clipboard-from-neovim
+if vim.fn.has("wsl") == 1 then
+  if vim.fn.executable("wl-copy") == 0 then
+    print("wl-clipboard not found, clipboard integration won't work")
+  else
+    vim.g.clipboard = {
+      name = "wl-clipboard (wsl)",
+      copy = {
+        ["+"] = "wl-copy --foreground --type text/plain",
+        ["*"] = "wl-copy --foreground --primary --type text/plain",
+      },
+      paste = {
+        ["+"] = function()
+          return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { "" }, 1) -- '1' keeps empty lines
+        end,
+        ["*"] = function()
+          return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { "" }, 1)
+        end,
+      },
+      cache_enabled = true,
+    }
+  end
+end
