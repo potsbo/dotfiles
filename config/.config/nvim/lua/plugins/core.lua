375a694c4829e3e0afafb98e784ee0b5395f32e8
Shimpei Otsubo <potsbo@gmail.com>
2026-02-12 11:56:14 +0900

    Update

--- config/.config/nvim/lua/plugins/core.lua
+++ config/.config/nvim/lua/plugins/core.lua
@@ -3,6 +3,11 @@ return {
     "loctvl842/monokai-pro.nvim",
     opts = {
       filter = "classic",
+      override = function()
+        return {
+          ["@lsp.type.parameter"] = { link = "@variable.parameter" },
+        }
+      end,
     },
   },
   {
@@ -10,6 +15,23 @@ return {
     opts = {
       colorscheme = "monokai-pro",
     },
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      -- VSCode bracket pair colorization colors
+      vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#FFD700" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#DA70D6" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#179FFF" })
+      local rainbow = require("rainbow-delimiters")
+      vim.g.rainbow_delimiters = {
+        highlight = {
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterBlue",
+        },
+      }
+    end,
   },
   -- ステータスラインを無効化
   {
