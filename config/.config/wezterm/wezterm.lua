1427d1ef3d6653aec30189a18dff2e17b0abf5ee
Shimpei Otsubo <potsbo@gmail.com>
2026-03-10 22:50:30 +0900

    Add wezterm config

---
+++ config/.config/wezterm/wezterm.lua
@@ -0,0 +1,41 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Theme
+config.color_scheme = "Monokai Remastered"
+
+-- Font
+config.font_size = 13.0
+config.font = wezterm.font_with_fallback({
+  { family = "JetBrains Mono", weight = "Medium", harfbuzz_features = { "calt=0", "liga=0", "dlig=0" } },
+  "Symbols Nerd Font Mono",
+  "BIZ UDGothic",
+})
+
+-- macOS: left Option as Alt, right Option as compose (default macOS behavior)
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = true
+
+-- Hide title bar (macos-titlebar-style = hidden)
+config.window_decorations = "NONE"
+
+-- Hide tab bar
+config.enable_tab_bar = false
+
+-- Remove window padding
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- Keybindings (neovim integration)
+config.keys = {
+  { key = "p", mods = "SUPER", action = wezterm.action.SendString("\x1b:CommandP\r") },
+  { key = "s", mods = "SUPER", action = wezterm.action.SendString("\x1b:w\r") },
+  { key = "\\", mods = "SUPER", action = wezterm.action.SendString("\x1b:vsplit\r") },
+  { key = "r", mods = "SUPER", action = wezterm.action.SendString("\x1b:e!\r") },
+}
+
+return config
